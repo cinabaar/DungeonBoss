1f5e2f6c0d1ba6de24befa96dae63f359fd3bedd
Dawid Romanowski <dawid.t.romanowski@gmail.com>
2016-05-18 20:33:33 +0200

    Initial commit

---
+++ Assets/Scripts/FlyingMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class FlyingMovement : MonoBehaviour
+{
+
+    public float Speed = 10;
+
+    private Rigidbody2D _rigidBody;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    _rigidBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+	    var horizontal = Input.GetAxis("Horizontal");
+	    var vertical = Input.GetAxis("Vertical");
+        //Debug.Log(horizontal + " " + vertical);
+	    var move = new Vector2(horizontal, vertical);
+	    _rigidBody.velocity = Speed*move;
+
+	}
+}
---
+++ Assets/Scripts/ForceCameraAspect.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForceCameraAspect : MonoBehaviour
+{
+    public float CameraWidthRatio = 16;
+    public float CameraHeightRatio = 9;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    var targetAspect = CameraWidthRatio/CameraHeightRatio;
+	    var windowAspect = (float)Screen.width/Screen.height;
+	    var scaleHeight = windowAspect/targetAspect;
+
+	    var camera = GetComponent<Camera>();
+
+	    if (scaleHeight < 1.0f)
+	    {
+	        var rect = camera.rect;
+	        rect.width = 1.0f;
+	        rect.height = scaleHeight;
+	        rect.x = 0;
+	        rect.y = (1.0f - scaleHeight)/2.0f;
+
+	        camera.rect = rect;
+	    }
+	    else
+	    {
+            var scalewidth = 1.0f / scaleHeight;
+            var rect = camera.rect;
+            rect.width = scalewidth;
+            rect.height = 1.0f;
+            rect.x = (1.0f - scalewidth) / 2.0f;
+            rect.y = 0;
+
+            camera.rect = rect;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
