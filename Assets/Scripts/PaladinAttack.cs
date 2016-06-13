e309ba2fc9d732e990c3fd2675fcadc2723734e4
Dawid Romanowski <dawid.t.romanowski@gmail.com>
2016-06-13 21:22:39 +0200

    nie chce mi sie juz

--- Assets/Scripts/PaladinAttack.cs
+++ Assets/Scripts/PaladinAttack.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,19 @@ using UnityEngine;
 
 class PaladinAttack : AEnemyAttack
 {
+    public Animator Animator;
+
+    public float Damage = 50;
+
     public override void Attack(GameObject who)
     {
+        Animator.SetBool("Attack", true);
+        StartCoroutine(EndAnimation());
+    }
+
+    private IEnumerator EndAnimation()
+    {
+        yield return new WaitForSeconds(0.1f);
+        Animator.SetBool("Attack", false);
     }
 }
---
+++ Assets/Scripts/Sword.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sword : MonoBehaviour {
+
+    public float Damage = 50;
+
+    public bool _lockDamage = false;
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Joe" && !_lockDamage)
+        {
+            var stats = other.gameObject.GetComponent<HeroStats>();
+            stats.Health -= Damage;
+            _lockDamage = true;
+            StartCoroutine(UnlockAttack());
+        }
+    }
+
+    IEnumerator UnlockAttack()
+    {
+        yield return new WaitForSeconds(1);
+        _lockDamage = false;
+    }
+}
