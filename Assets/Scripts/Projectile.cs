4675ad47ace66d6320f75e044b170dd97f766ac3
Dawid Romanowski <dawid.t.romanowski@gmail.com>
2016-06-13 20:20:54 +0200

    Wizard attack deals damage

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour
+{
+    public float Damage = 50;
+
+    private bool _damageDealt = false;
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Joe" && !_damageDealt)
+        {
+            var stats = other.gameObject.GetComponent<HeroStats>();
+            stats.Health -= Damage;
+            _damageDealt = true;
+        }
+        Destroy(this.gameObject);
+
+    }
+}
